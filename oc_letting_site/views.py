b017bde2ba3af012c81ebdb3907e5a47750f766d
Mickael Salmon <mickael.salmon@outlook.com>
2023-11-19 22:07:14 +0100

    sync updated requirements

--- oc_letting_site/views.py
+++ oc_letting_site/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
+    """
+    Renders the index page of the website.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered HTML response.
+    """
     # logique de la vue
     return render(request, "index.html")
