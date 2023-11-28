02f6214662b8b054fde488a147ffe8f7cc635c97
Mickael Salmon <mickael.salmon@outlook.com>
2023-11-28 19:29:56 +0100

    Added collectstatic in the docker file

--- oc_letting_site/settings.py
+++ oc_letting_site/settings.py
@@ -131,16 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / "static", ]
-
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
