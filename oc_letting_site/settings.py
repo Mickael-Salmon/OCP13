725e5cfeb274832ec6e5e14291928708452a5d0c
Mickael Salmon <mickael.salmon@outlook.com>
2023-11-28 18:47:58 +0100

    Added Whitenoise for static files

--- oc_letting_site/settings.py
+++ oc_letting_site/settings.py
@@ -131,10 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = "/static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = 'staticfiles/'
+STATICFILES_DIRS = [BASE_DIR / "static", ]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
