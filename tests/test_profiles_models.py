4aeff12f9625d6026789c22b6682e3a38f880108
Mickael Salmon <mickael.salmon@outlook.com>
2023-11-28 11:11:40 +0100

    tests

--- tests/test_profiles_models.py
+++ tests/test_profiles_models.py
@@ -14,19 +14,19 @@ class ProfileModelTest(TestCase):
     def test_user_label(self):
         profile = Profile.objects.get(id=1)
         field_label = profile._meta.get_field('user').verbose_name
-        self.assertEquals(field_label, 'user')
+        self.assertEqual(field_label, 'user')
 
     def test_favorite_city_label(self):
         profile = Profile.objects.get(id=1)
         field_label = profile._meta.get_field('favorite_city').verbose_name
-        self.assertEquals(field_label, 'favorite city')
+        self.assertEqual(field_label, 'favorite city')
 
     def test_favorite_city_max_length(self):
         profile = Profile.objects.get(id=1)
         max_length = profile._meta.get_field('favorite_city').max_length
-        self.assertEquals(max_length, 64)
+        self.assertEqual(max_length, 64)
 
     def test_object_name_is_username(self):
         profile = Profile.objects.get(id=1)
         expected_object_name = f'{profile.user.username}'
-        self.assertEquals(expected_object_name, str(profile))
+        self.assertEqual(expected_object_name, str(profile))
