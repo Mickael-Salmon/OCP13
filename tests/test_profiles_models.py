1f3a3f4b5013fbaf1c93722a3c040acce9b97f31
Mickael Salmon <mickael.salmon@outlook.com>
2023-11-28 19:43:32 +0100

    added a test

--- tests/test_profiles_models.py
+++ tests/test_profiles_models.py
@@ -2,23 +2,30 @@
 from django.contrib.auth import get_user_model
 from profiles.models import Profile
 
-# Set up non-modified objects used by all test methods
-user = get_user_model().objects.create_user(username='testuser', password='testpass')
-Profile.objects.create(user=user, favorite_city='San Francisco')
+class ProfileModelTest(TestCase):
 
-# Test the label of the 'user' field
-profile = Profile.objects.get(id=1)
-field_label = profile._meta.get_field('user').verbose_name
-assert field_label == 'user'
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        cls.profile = Profile.objects.create(user=cls.user, favorite_city='San Francisco')
 
-# Test the label of the 'favorite_city' field
-field_label = profile._meta.get_field('favorite_city').verbose_name
-assert field_label == 'favorite city'
+    def test_user_label(self):
+        # Test the label of the 'user' field
+        field_label = self.profile._meta.get_field('user').verbose_name
+        self.assertEqual(field_label, 'user')
 
-# Test the max length of the 'favorite_city' field
-max_length = profile._meta.get_field('favorite_city').max_length
-assert max_length == 64
+    def test_favorite_city_label(self):
+        # Test the label of the 'favorite_city' field
+        field_label = self.profile._meta.get_field('favorite_city').verbose_name
+        self.assertEqual(field_label, 'favorite city')
 
-# Test the object name of the profile
-expected_object_name = f'{profile.user.username}'
-assert expected_object_name == str(profile)
+    def test_favorite_city_max_length(self):
+        # Test the max length of the 'favorite_city' field
+        max_length = self.profile._meta.get_field('favorite_city').max_length
+        self.assertEqual(max_length, 64)
+
+    def test_object_name(self):
+        # Test the object name of the profile
+        expected_object_name = f'{self.profile.user.username}'
+        self.assertEqual(str(self.profile), expected_object_name)
